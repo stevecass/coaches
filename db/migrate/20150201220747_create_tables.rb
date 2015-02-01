aeaf6a1accaade05d7a28ab716b2df9153c9e176
Steven Cassidy <steven@mumsnet.com>
2015-02-01 17:45:16 -0500

    Create tables

--- db/migrate/20150201220747_create_tables.rb
+++ db/migrate/20150201220747_create_tables.rb
@@ -1,6 +1,26 @@
 class CreateTables < ActiveRecord::Migration
   def change
-    create_table :tables do |t|
+
+    create_table :publications do |t|
+      t.string :title
+      t.string :frequency
+      t.string :imprint
     end
+
+    create_table :users do |t|
+      t.string :email
+      t.string :password_digest
+      t.string :name
+      t.date :dob
+    end
+
+    create_table :subscriptions do |t|
+      t.integer :user_id
+      t.integer :publication_id
+      t.date :start_date
+      t.date :end_date
+      t.string :payment_status
+    end
+
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150201220747) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "publications", force: :cascade do |t|
+    t.string "title"
+    t.string "frequency"
+    t.string "imprint"
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "publication_id"
+    t.date    "start_date"
+    t.date    "end_date"
+    t.string  "payment_status"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.string "name"
+    t.date   "dob"
+  end
 
 end
