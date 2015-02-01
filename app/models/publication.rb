282f59e86de440afc6b0e3b957644af36d3ac667
Steven Cassidy <steven@mumsnet.com>
2015-02-01 18:17:51 -0500

    Add a subscribers association to publication

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,4 +1,5 @@
 class Publication < ActiveRecord::Base
   has_many :subscriptions
   has_many :users, through: :subscriptions
+  has_many :subscribers, :through => :subscriptions, :source => :user
 end
