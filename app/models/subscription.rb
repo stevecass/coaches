ae48c380dc668ffc48efac78206a8d7e1d096b19
Steven Cassidy <steven@mumsnet.com>
2015-02-01 18:18:05 -0500

    Add a nonsense association to subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,5 +1,6 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
+  belongs_to :jimmylala, class_name: 'User', foreign_key: :user_id
   belongs_to :publication
   scope :paid, -> {where(payment_status: 'paid')}
 end
