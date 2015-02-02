8f0de51175f6c7b3c57d1991b4762856066df29d
Steven Cassidy <steven@mumsnet.com>
2015-02-02 12:00:53 -0500

    Add generated seeds.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Publication.create!([
+  {id: 1, title: "Vogue", frequency: "monthly", imprint: "Conde Nast"}
+])
+Subscription.create!([
+  {id: 1, user_id: 1, publication_id: 1, start_date: nil, end_date: nil, payment_status: nil},
+  {id: 2, user_id: 1, publication_id: 1, start_date: "2015-01-01", end_date: "2015-12-31", payment_status: "paid"}
+])
+User.create!([
+  {id: 1, email: "steven@cassidy.co.uk", password_digest: "$2a$10$RECcc0l0foHz9XXBaDW/me9UoOWsYbTAedfZRGqF7grwZi.1qlk9e", name: "Steven Cassidy", dob: "1967-03-04"}
+])
