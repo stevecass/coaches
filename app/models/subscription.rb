1bb3d6b9dc7822b38313b872f77078b7f1f524dc
stevecass <steven@mumsnet.com>
2015-02-02 16:52:25 -0500

    Add nonsense belongs_to notes

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,9 @@
 class Subscription < ActiveRecord::Base
   belongs_to :user
-  belongs_to :jimmylala, class_name: 'User', foreign_key: :user_id
+
+  # These names are just labels that add methods
+  # belongs_to :jimmylala, class_name: 'User', foreign_key: :user_id
+
   belongs_to :publication
   scope :paid, -> {where(payment_status: 'paid')}
 end
