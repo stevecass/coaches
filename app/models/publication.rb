1c5526897da3ee5bfe4f1e4867f60bb7b60e7895
Steven Cassidy <steven@mumsnet.com>
2015-02-02 14:05:15 -0500

    Add paid_subs methods to publication

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -2,4 +2,12 @@ class Publication < ActiveRecord::Base
   has_many :subscriptions
   has_many :users, through: :subscriptions
   has_many :subscribers, :through => :subscriptions, :source => :user
+
+  def paid_subscriptions
+    Subscription.paid.where(publication: self)
+  end
+
+  def paid_subscribers
+    User.joins(:subscriptions).merge(Subscription.paid).where('subscriptions.publication_id = ?',  1)
+  end
 end
